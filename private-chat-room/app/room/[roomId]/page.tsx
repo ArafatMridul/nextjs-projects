31bbadaa809357fc87fea83ac3e2a111dede2eb3
arafat <mridularafat735@gmail.com>
2026-01-15 20:26:00 +0600

    messaging page implementation

---
+++ private-chat-room/app/room/[roomId]/page.tsx
@@ -0,0 +1,91 @@
+"use client"
+
+import {useParams} from "next/dist/client/components/navigation";
+import {useRef, useState} from "react";
+
+const formatTimeRemaining = (seconds: number) => {
+    const mins = Math.floor(seconds / 60);
+    const secs = seconds % 60;
+
+    return `${mins}:${secs.toString().padStart(2, "0")}`;
+}
+
+const Page = () => {
+    const [copied, setCopied] = useState(false);
+    const [timeRemaining, setTimeRemaining] = useState<number | null>(121)
+    const [input, setInput] = useState("");
+    const inputRef = useRef<HTMLInputElement>(null);
+
+    const {roomId} = useParams();
+
+    const copyText = async () => {
+        const url = window.location.href;
+        await navigator.clipboard.writeText(url);
+        setCopied(true);
+        setTimeout(() => {
+            setCopied(false);
+        }, 2000);
+    }
+
+    return (
+        <main className={"h-screen flex flex-col"}>
+            <header className={"py-6 border-b border-zinc-700"}>
+                <div className={"max-w-5xl mx-auto flex items-center gap-10"}>
+                    <div className={"pr-10 border-r border-dashed border-zinc-600"}>
+                        ROOM ID
+                        <div className={"flex items-center gap-3"}>
+                            <p className={"text-green-500 text-xl font-bold"}>{roomId}</p>
+                            <div className={"w-12 mx-auto"}>
+                                <button
+                                    onClick={copyText}
+                                    className={`border px-3 py-1.5 text-[0.5rem] font-extrabold transition-colors cursor-pointer ${copied ? "bg-zinc-500" : "bg-zinc-800"}`}>{copied ? "COPIED!" : "COPY"}</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={"flex items-center justify-between flex-1"}>
+                        <div>
+                            <p>SELF-DESTRUCT IN</p>
+                            <span
+                                className={timeRemaining !== null && timeRemaining > 60 ? "text-yellow-500" : "text-red-400"}>{timeRemaining !== null ? formatTimeRemaining(timeRemaining) : "--:--"}</span>
+                        </div>
+                        <div>
+                            <button
+                                className={"ring-2 border-none ring-transparent font-extrabold hover:ring-red-200 transition-all duration-300 ease-in-out cursor-pointer px-8 py-2 bg-red-500"}>DESTROY
+                                NOW
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </header>
+            <section className={"flex-1"}>
+                {/*    MESSAGING SECTION    */}
+            </section>
+
+            <footer className={"bg-zinc-900 py-6"}>
+                <div className={"max-w-5xl mx-auto flex items-center gap-4"}>
+                    <div className={"flex-1 flex items-center bg-black"}>
+                        <div className={"flex font-extrabold"}>
+                            <span className={"px-2 text-green-500"}>{"~ "}</span>
+                            <span className={"text-green-500"}>{"> "}</span>
+                        </div>
+                        <input autoFocus ref={inputRef} type="text" value={input}
+                               onKeyDown={(e) => {
+                                   if (e.key === "Enter" && input.trim()) {
+                                       // send message
+                                       inputRef.current?.focus();
+                                   }
+                               }}
+                               placeholder={"type message..."}
+                               onChange={(e) => setInput(e.target.value)}
+                               className={"px-4 py-3 text-xl w-full focus:outline-none"}/>
+                    </div>
+                    <button
+                        className={"px-6 py-2 border border-zinc-400 bg-zinc-600 hover:bg-zinc-500 cursor-pointer transition-colors"}>SEND
+                    </button>
+                </div>
+            </footer>
+        </main>
+    );
+};
+
+export default Page;
