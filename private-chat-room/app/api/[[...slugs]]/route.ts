053abdaa5d152161cc895e63cbfc140b99c8f4a3
arafat <mridularafat735@gmail.com>
2026-01-16 08:59:01 +0600

    messaging route setup

--- private-chat-room/app/api/[[...slugs]]/route.ts
+++ private-chat-room/app/api/[[...slugs]]/route.ts
@@ -1,6 +1,9 @@
 import {Elysia} from 'elysia'
 import {nanoid} from "nanoid";
 import {redis} from "@/lib/Redis";
+import {authMiddleWare} from "@/app/api/[[...slugs]]/auth";
+import {Message, realtime} from "@/lib/realtime";
+import z from "zod";
 
 const ROOM_TTL_SECS = 60 * 10; // time-to-live(ttl) : 10 mins
 
@@ -16,7 +19,44 @@ const rooms = new Elysia({prefix: "/room"}).post("/create", async () => {
     return {roomId};
 })
 
-export const app = new Elysia({prefix: '/api'}).use(rooms)
+const messages = new Elysia({prefix: "/messages"}).use(authMiddleWare).post("/", async ({body, auth}) => {
+    const {roomId} = auth;
+    const {sender, text}: { sender: string, text: string } = body;
+
+    const roomExists = await redis.exists(`meta:${roomId}`);
+    if (!roomExists) {
+        throw new Error("Room does not exists");
+    }
+
+    const message: Message = {
+        id: nanoid(),
+        sender,
+        text,
+        timestamp: Date.now(),
+        roomId,
+    }
+
+    await redis.rpush(`messages:${roomId}`, {
+        ...message, token: auth.token
+    })
+    // @ts-expect-error
+    await realtime.channel(roomId).emit("chat.message", message);
+    const remaining = await redis.ttl(`meta:${roomId}`);
+    await redis.expire(`messages:${roomId}`, remaining);
+    await redis.expire(`history:${roomId}`, remaining);
+    await redis.expire(roomId, remaining);
+
+}, {
+    body: z.object({
+        sender: z.string().max(100),
+        text: z.string().max(1000),
+    }),
+    query: z.object({
+        roomId: z.string(),
+    })
+})
+
+export const app = new Elysia({prefix: '/api'}).use(rooms).use(messages)
 
 export const GET = app.fetch
 export const POST = app.fetch
