4073cccd745ce45af91396507204a01950040034
arafat <mridularafat735@gmail.com>
2026-01-31 04:20:49 +0600

    add signup schema

---
+++ blog-site/schemas/signupSchema.ts
@@ -0,0 +1,11 @@
+import z from 'zod';
+
+export const signupSchema = z.object({
+    fullName: z.string().min(2, "Full name must be at least 2 characters"),
+    email: z.email("Please enter a valid email address"),
+    password: z.string().min(8, "Password must be at least 8 characters"),
+    confirmPassword: z.string(),
+}).refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords don't match",
+    path: ["confirmPassword"],
+});
